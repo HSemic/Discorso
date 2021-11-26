9a64b35e4a52579a32efa70d14378ab70b0fa49e
HarisSemic <68331529+HSemic@users.noreply.github.com>
2021-11-26 02:17:11 +0100

    Update test_endpoints.py

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -1,3 +1,7 @@
 def test_square(client):
-    rv = client.get("/square?number=8")
-    assert b"64" == rv.data
+    data = client.get("/aiml")
+    assert data["message"] == "This is a test message from the nlp chatbot."
+
+def test_square(client):
+    data = client.get("/nlp")
+    assert data["message"] == "This is a test message from the nlp chatbot."
